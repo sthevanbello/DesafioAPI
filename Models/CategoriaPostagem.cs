5e341a58322bac0e832b6c64c4a044a77b7cd122
sthevanbello <sthevan.alves@gmail.com>
2022-08-26 16:37:55 -0300

    Criada a Model CategoriaPostagem

---
+++ Models/CategoriaPostagem.cs
@@ -0,0 +1,9 @@
+﻿namespace ForumGames.Models
+{
+    // Categoria das postagens
+    public class CategoriaPostagem
+    {
+        public int Id { get; set; }
+        public string NomeCategoria { get; set; }
+    }
+}
