9d183b99162e1ff459d228162039af74869e2b17
sthevanbello <sthevan.alves@gmail.com>
2022-08-26 17:50:33 -0300

    Criada a model de JogadorGrupo

---
+++ Models/JogadorGrupo.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ForumGames.Models
+{
+    public class JogadorGrupo
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Informe o Id do grupo")]
+        public int GrupoId { get; set; }
+        public Grupo Grupo { get; set; }
+        [Required(ErrorMessage = "Informe o Id do jogador")]
+        public int JogadorId { get; set; }
+        public Jogador Jogador { get; set; }
+    }
+}
