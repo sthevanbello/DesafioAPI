ccd3a5dc74054d89238f915f7d447af1c2b968fe
sthevanbello <sthevan.alves@gmail.com>
2022-08-26 16:36:07 -0300

    Criada a model CategoriaGrupo

---
+++ Models/CategoriaGrupo.cs
@@ -0,0 +1,9 @@
+﻿namespace ForumGames.Models
+{
+    // Categoria dos grupos
+    public class CategoriaGrupo
+    {
+        public int Id { get; set; }
+        public string NomeCategoria { get; set; }    
+    }
+}
