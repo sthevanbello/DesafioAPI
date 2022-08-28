2afe571826144777d2399d4087225ddc10b980e3
sthevanbello <sthevan.alves@gmail.com>
2022-08-28 15:37:33 -0300

    Modificada a interface com a adição de GetJogadorPorIdComPostagens e  GetJogadoresComPostagens

--- Interfaces/IJogadorRepository.cs
+++ Interfaces/IJogadorRepository.cs
@@ -7,8 +7,10 @@ namespace ForumGames.Interfaces
     {
         public ICollection<Jogador> GetJogadores();
         public Jogador GetJogadorPorId(int id);
-        public Jogador GetJogadorPorIdComGrupos(int id);
         public ICollection<Jogador> GetJogadoresComGrupos();
+        public Jogador GetJogadorPorIdComGrupos(int id);
+        public ICollection<Jogador> GetJogadoresComPostagens();
+        public Jogador GetJogadorPorIdComPostagens(int id);
         public Jogador InsertJogador(Jogador jogador);
         public Jogador InsertJogadorComImagem(Jogador jogador);
         public bool UpdateJogador(int id, Jogador jogador);
