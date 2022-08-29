e7e2d0d66496503ccb184c82cbf80f06338f9ceb
sthevanbello <sthevan.alves@gmail.com>
2022-08-29 14:47:56 -0300

    Criada uma classe de Exceção personalizada para Categoria

---
+++ Utils/Exceptions/ThereIsntCategoryException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ForumGames.Utils.Exceptions
+{
+    public class ThereIsntCategoryException : ApplicationException
+    {
+        public ThereIsntCategoryException(string message) : base(message)
+        {
+
+        }
+    }
+}
