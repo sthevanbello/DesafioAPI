a2a2e4415ca20903960525d019bd6c1bd6fd2ddc
sthevanbello <sthevan.alves@gmail.com>
2022-08-29 11:08:37 -0300

    Criado o método GetAllCategoriaGrupoComGrupos

--- Repositories/CategoriaGrupoRepository.cs
+++ Repositories/CategoriaGrupoRepository.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Data;
 using ForumGames.Utils.Exceptions;
+using System.Text.RegularExpressions;
+using System.Linq;
 
 namespace ForumGames.Repositories
 {
@@ -39,7 +41,7 @@ namespace ForumGames.Repositories
         }
 
         /// <summary>
-        /// Lista todas as categorias de grupos cadastradas
+        /// Listar todas as categorias de grupos cadastradas
         /// </summary>
         /// <returns>Retorna todas as categorias de grupos cadastradas</returns>
         public ICollection<CategoriaGrupo> GetAllCategoriaGrupo()
@@ -112,7 +114,71 @@ namespace ForumGames.Repositories
             }
             return categoriaGrupo;
         }
+        /// <summary>
+        /// Listar todas as categorias de grupos com seus respectivos grupos
+        /// </summary>
+        /// <returns>todas as categorias de grupos com seus respectivos grupos</returns>
+        public IList<CategoriaGrupo> GetAllCategoriaGrupoComGrupos()
+        {
+            IList<CategoriaGrupo> listaCategoriaGrupo = new List<CategoriaGrupo>();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string script = @"SELECT 
+                                    CG.Id AS 'Id_Categoria',
+                                    CG.Categoria AS 'Categoria_Nome',
+	                                G.CategoriaId AS 'ID_Categoria_Grupo',
+	                                G.Id AS 'Id_Grupo',
+	                                G.Descricao AS 'Grupo_Descricao'
+                                FROM TB_Categorias_Grupos AS CG
+                                LEFT JOIN TB_Grupos AS G ON CG.Id = G.CategoriaId";
+                using (SqlCommand cmd = new SqlCommand(script, connection))
+                {
+                    // Ler todos os itens da consulta com foreach e while
+                    cmd.CommandType = CommandType.Text;
+                    using (var result = cmd.ExecuteReader())
+                    {
+                        while (result != null && result.HasRows && result.Read())
+                        {
+                            var grupo = new Grupo();
+                            if (!string.IsNullOrEmpty(result["Id_Grupo"].ToString()))
+                            {
+                                grupo = new Grupo
+                                {
+                                    Id = (int)result["Id_Grupo"],
+                                    Categoria = null,
+                                    Descricao = result["Grupo_Descricao"].ToString() == "" ? null : result["Grupo_Descricao"].ToString(),
+                                    Jogadores = null
+                                };
 
+                            }
+                            if (!listaCategoriaGrupo.Any(x => x.Id == (int)result["Id_Categoria"]))
+                            {
+                                var categoriaGrupo = new CategoriaGrupo
+                                {
+                                    Id = (int)result["Id_Categoria"],
+                                    NomeCategoriaGrupo = (string)result["Categoria_Nome"],
+                                };
+
+                                if ((categoriaGrupo?.Id ?? 0) > 0)
+                                {
+                                    categoriaGrupo.Grupos.Add(grupo);
+                                }
+
+                                listaCategoriaGrupo.Add(categoriaGrupo);
+                            }
+                            else if ((grupo?.Id ?? 0) > 0) // grupo?.Id ?? 0 -> Garante que se for nulo, atribui o valor 0 e compara se é maior do que zero.
+                            {
+                                // Busca o jogador e adiciona o grupo na lista de grupos dos quais o jogador participa
+                                listaCategoriaGrupo.FirstOrDefault(x => x.Id == (int)result["Id_Categoria"]).Grupos.Add(grupo);
+                            }
+
+                        }
+                    }
+                }
+            }
+            return listaCategoriaGrupo;
+        }
         /// <summary>
         /// Exibe uma única categoria a partir do Id fornecido como parâmetro e exibe de quais grupos ela faz parte
         /// </summary>
@@ -165,7 +231,7 @@ namespace ForumGames.Repositories
             return categoriaGrupo;
         }
         /// <summary>
-        /// Atualiza uma categoria de acordo com o Id fornecido
+        /// Atualizar uma categoria de acordo com o Id fornecido
         /// </summary>
         /// <param name="id">Id da categoriaa ser atualizada</param>
         /// <param name="categoriaGrupo">Dados atualizados</param>
@@ -197,6 +263,12 @@ namespace ForumGames.Repositories
             }
             return true;
         }
+        /// <summary>
+        /// Excluir uma categoria se não houver um grupo associado
+        /// </summary>
+        /// <param name="id">Id da categoria de grupo</param>
+        /// <returns>Retorna se a categoria foi alterada ou não foi excluída</returns>
+        /// <exception cref="CannotDeleteException">Captura a exceção caso não seja possível excluir o item</exception>
         public bool DeleteCategoriaGrupo(int id)
         {
             var categoriaGrupo = GetCategoriaGrupoPorIdComGrupos(id);
@@ -226,5 +298,6 @@ namespace ForumGames.Repositories
             }
             return true;
         }
+
     }
 }
